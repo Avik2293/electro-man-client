a0be81c5b699692505298e48807dec890f8eb5bf
Avik2293 <aviksarker22@gmail.com>
2022-10-25 23:06:12 +0600

    course details done

--- src/Components/CourseCard.js
+++ src/Components/CourseCard.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import { FaStar } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 const CourseCard = (props) => {
-    console.log(props.course);
+    // console.log(props.course);
     return (
         <div className="card lg:card-side bg-base-100 shadow-xl my-3">
             <figure><img className='h-auto' src={props.course.image_url} alt="Album" /></figure>
@@ -11,11 +13,11 @@ const CourseCard = (props) => {
                 <div>
                     <div className='flex space-x'>
                         <p className='border-2 border-green-500 rounded-lg m-1'><span className='font-bold'>Instructor:</span> {props.course.instructor}</p>
-                        <p className='border-2 border-green-500 rounded-lg m-1'><span className='font-bold'>Ratings:</span> {props.course.rating}</p>
+                        <p className='border-2 border-green-500 rounded-lg m-1 flex'><FaStar className='m-1'></FaStar><span className='font-bold'>Ratings:</span> {props.course.rating}</p>
                         <p className='border-2 border-green-500 rounded-lg m-1'><span className='font-bold'>Duration:</span> {props.course.duration} Hours</p>
                     </div>
                     <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Details</button>
+                        <button className="btn btn-primary"><Link to={`/course/${props.course.id}`}>Details</Link></button>
                     </div>
                 </div>
             </div>
--- src/Pages/Course.js
+++ src/Pages/Course.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import { FaStar } from 'react-icons/fa';
+import { useLoaderData } from 'react-router-dom';
 
 const Course = () => {
+
+    const courseInfo = useLoaderData();
+    console.log(courseInfo);
     return (
         <div>
-            
+            <div className='mt-2'>
+                <button className='btn btn-info mr-4'>Download PDF</button>
+                <button className='btn btn-secondary ml-4'>Get Premium Access</button>
+            </div>
+            <div className='border-4 border-teal-600 bg-sky-200 rounded-xl m-2 p-3'>
+                <div className='flex justify-center m-3'>
+                    <img className='' src={courseInfo.image_url} alt=''></img>
+                    <div className='grid content-evenly p-3'>
+                        <p className='border-2 border-green-500 rounded-lg m-1'><span className='font-bold'>Instructor:</span> {courseInfo.instructor}</p>
+                        <p className='border-2 border-green-500 rounded-lg m-1 flex'><FaStar className='m-1'></FaStar><span className='font-bold'>Ratings:</span> {courseInfo.rating}</p>
+                        <p className='border-2 border-green-500 rounded-lg m-1'><span className='font-bold'>Duration:</span> {courseInfo.duration} Hours</p>
+                    </div>
+                </div>
+                <h2 className='text-3xl font-bold'>{courseInfo.title}</h2>
+                <p className='text-2xl'>{courseInfo.details}</p>
+            </div>
         </div>
     );
 };
