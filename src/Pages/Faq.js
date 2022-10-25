9d9ed9e50aaeee49eafc924d30cec025ffb583ce
Avik2293 <aviksarker22@gmail.com>
2022-10-25 17:12:47 +0600

    FAQ page

--- src/Pages/Faq.js
+++ src/Pages/Faq.js
@@ -7,36 +7,55 @@ const Faq = () => {
         <div>
             <Header></Header>
             <div>
+                <h2 className='text-3xl font-bold'>Some Frequently Asked Question in here....</h2>
                 <div tabIndex={0} className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box lg:mx-48 my-2">
                     <div className="collapse-title text-xl font-medium">
-                        Focus me to see content
+                        What Is Electrical Engineering?
                     </div>
                     <div className="collapse-content">
-                        <p>tabIndex={0} attribute is necessary to make the div focusable</p>
+                        <p>
+                            Electrical Engineering is a section of the engineering discipline that deals with electric systems, electricity, and electromagnetism. Electrical engineers use the latest tools in computer science as well as more old-fashioned disciplines to build everything from electric cars to massive electrical infrastructures and control systems. It's a relatively new field within engineering, but it's been around since the advent of electric commercialization in the 19th century. It's divided into a wide range of subfields, including traditional ones such as signal processing and newer ones like computer engineering.
+                        </p>
                     </div>
                 </div>
                 <div tabIndex={0} className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box lg:mx-48 my-2">
                     <div className="collapse-title text-xl font-medium">
-                        Focus me to see content
+                        What Do Electrical Engineers Do?
                     </div>
                     <div className="collapse-content">
-                        <p>tabIndex={0} attribute is necessary to make the div focusable</p>
+                        <p>
+                            Electrical Engineers use knowledge of circuitry and power generation to plan, design, and manage the manufacturing of electrical equipment as well as implement projects that harness different components of electricity and power. They use their knowledge of electrical systems to work in projects so small they could fit into your pocket to larger projects like aircraft electrical systems or communications systems. They design and test their projects, ensuring that everything created is safe. Careers In Electrical Engineering The field is still lucrative with the advent of 21st-century computing technology and electrical devices like smartphones and computer systems. There's also an extensive opportunity within biomedical engineering. You can find space for an electrical engineer in a wide range of fields.
+                        </p>
                     </div>
                 </div>
                 <div tabIndex={0} className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box lg:mx-48 my-2">
                     <div className="collapse-title text-xl font-medium">
-                        Focus me to see content
+                        Learn Electrical Engineering
                     </div>
                     <div className="collapse-content">
-                        <p>tabIndex={0} attribute is necessary to make the div focusable</p>
+                        <p>
+                            You'll need at least a bachelor's degree to get a foot in the door, but most lucrative positions will require a graduate degree with master's degree at a minimum and a strong preference towards PhDs. Degree programs can be quite an investment, but they're a requirement for working within the field. You'll also need a fair amount of work experience to land the most lucrative jobs.
+                        </p>
                     </div>
                 </div>
                 <div tabIndex={0} className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box lg:mx-48 my-2">
                     <div className="collapse-title text-xl font-medium">
-                        Focus me to see content
+                        Online Electrical Engineering Courses And Certifications
                     </div>
                     <div className="collapse-content">
-                        <p>tabIndex={0} attribute is necessary to make the div focusable</p>
+                        <p>
+                            The field is broad, and so are the offerings. You can start from the beginning with an introduction to basic engineering principles in areas such as electrical, mechanical, and biomedical engineering from the University of Texas at Arlington. You can also go to the other end of the spectrum with field-specific courses in things like Future Robotics from the University of Naples. In between are a wide range of certifications and courses designed to build foundation skills in electrical engineering, including principles like circuit theory, systems design, and power generation. Understanding the basis of electric power allows you to apply those principles to innovative new projects within the newest tech horizon. Courses in computer-aided design also give you modern tools to work with current projects in the professional engineering world. Even if you already have your degree, specialized certificates and courses expand your tool belt and put you ahead of the competition.
+                        </p>
+                    </div>
+                </div>
+                <div tabIndex={0} className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box lg:mx-48 my-2">
+                    <div className="collapse-title text-xl font-medium">
+                        Explore A Career In Electrical Engineering
+                    </div>
+                    <div className="collapse-content">
+                        <p>
+                            This isn't your 19th-century power grid anymore. It's a lucrative field full of possibilities for innovation with the far reaches of tech. Electronic devices are getting more critical to everyday life. Electric motors are revolutionizing the automobile industry, and electronic systems are getting small enough to require a microscope to see. Real world technological innovation is expanding, and an electrical engineering degree gives you the right skills to get in the field. The Bureau of Labor Statistics estimates the average pay at over six figures and growing, and that's excluding computer engineering careers. This is the time to consider becoming a professional engineer in the field of electrical engineering and being part of the tech wave.
+                        </p>
                     </div>
                 </div>
             </div>
