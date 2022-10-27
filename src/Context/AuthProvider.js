2204d44f149a7bc91835925f639b967d130635c2
Avik2293 <aviksarker22@gmail.com>
2022-10-27 06:35:06 +0600

    github login done

--- src/Context/AuthProvider.js
+++ src/Context/AuthProvider.js
@@ -55,7 +55,7 @@ const AuthProvider = ({children}) => {
         }
     }, [])
 
-    const authInfo = {user, providerLogin, signIn, createUser, logOut, loading, setLoading, updateUserProfile, verifyEmail};
+    const authInfo = {user, setUser, providerLogin, signIn, createUser, logOut, loading, setLoading, updateUserProfile, verifyEmail};
 
     return (
         <AuthContext.Provider value={authInfo}>
