01c55ac4befdd373fb8a332e109e34876369639d
Avik2293 <aviksarker22@gmail.com>
2022-12-25 22:29:18 +0600

    some correction

--- src/Components/StayConnect.js
+++ src/Components/StayConnect.js
@@ -18,7 +18,7 @@ const StayConnect = () => {
                     <div className="flex flex-row items-center self-center justify-center flex-shrink-0 shadow-md lg:justify-end">
                         <div className="flex flex-row">
                             <input type="text" placeholder="example@email.com" className="w-3/5 p-3 rounded-l-lg sm:w-2/3" />
-                            <button type="button" className="w-2/5 p-3 font-semibold rounded-r-lg sm:w-1/3 dark:bg-violet-400 dark:text-gray-900">Subscribe</button>
+                            <button type="button" className="w-2/5 p-3 font-semibold rounded-r-lg sm:w-1/3 dark:bg-violet-400 border-slate-400 border dark:text-gray-900">Subscribe</button>
                         </div>
                     </div>
                 </motion.button >
